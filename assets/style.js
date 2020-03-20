d73c3c5c9c611a32b213c1a68ce87a47e4577fd8
aphan <phantasticvs@yahoo.com>
2020-03-19 20:18:20 -0600

    Made minor tweaks in randomizer elements and loops (most of the testing being done is in a local file not being pushed up at this time.

--- assets/style.js
+++ assets/style.js
@@ -2,6 +2,7 @@ var startQuizBtn = document.getElementById("startQuizBtn");
 
 function beginQuiz() {
   var timeSelection = document.getElementsByName("duration");
+  
   // Takes user input from radio buttons, determines length of quiz in minutes and seconds:
   var quizDuration = setTime();
   function setTime() {
@@ -11,6 +12,19 @@ function beginQuiz() {
       }
     }
   };
+  
+  // The number of questions is derivative of the length of quiz:
+  // var numOfQuestions = Math.floor(quizDuration / 30);
+  
+  // function generateQuestion() {
+  //   var usedQuestions = [];
+  //   function selQuestions() {
+  //     for (i = 0; i > numOfQuestions; i++) {
+  //       usedQuestions[i] = Math.floor(Math.random() * );
+  //     }
+  //     return usedQuestions;
+  //   }
+  // };
   
   // Initiates and regulates the countdown timer:
   function startClock() {
@@ -35,9 +49,67 @@ function beginQuiz() {
       }
     };
   };
+
   setTime();
   startClock();
 }
 
 
 startQuizBtn.addEventListener("click", beginQuiz);
+
+
+
+// RESOURCES/POTENTIAL CODE:
+// var answerSequence;
+// var orderedList;
+
+// var question1 = [["Red", true], ["Green", false],["Purple", false],["All of the Above", false],["None of the Above", false], "What color is Anthony's favorite?"];
+
+// BETTER RANDOMIZER:
+// Randomizes the order of answer options, but only the first three, so as to maintain options 4 & 5 as "All/None of the Above", respectively:
+// function randomizeAnswers() {
+//   var numPool = [0, 1, 2];
+//   var index0 = Math.floor(Math.random() * 3);
+//   numPool.splice(index0, 1);
+//   var index1 = parseInt(numPool.splice(Math.floor(Math.random() * 2), 1));
+//   var index2 = numPool[0];
+//   answerSequence = [index0, index1, index2, 3, 4];
+// };
+
+// randomizeAnswers();
+
+
+// function formQuestion() {
+//   for (i = 0; i < 5; i++) {
+//     console.log(question1[answerSequence[i]][0]);
+//   }
+// };
+
+// SHUFFLERS:
+// function shuffle4(arr) {
+//   var newSpot;
+//   var placeholderSpot;
+//   for (i = 3; i > 0; i--) {
+//     newSpot = Math.floor(Math.random() * (i + 1));
+//     placeholderSpot = arr[i];
+//     arr[i] = arr[newSpot];
+//     arr[newSpot] = placeholderSpot;
+//   }
+//   return arr;
+// };
+
+// function shuffle5(arr) {
+//   var newSpot;
+//   var placeholderSpot;
+//   for (i = 4; i > 0; i--) {
+//     newSpot = Math.floor(Math.random() * (i + 1));
+//     placeholderSpot = arr[i];
+//     arr[i] = arr[newSpot];
+//     arr[newSpot] = placeholderSpot;
+//   }
+//   return arr;
+// };
+
+
+// SAMPLE QUESTION SET:
+// var question1 = [ { name: "red", isTrue: false }, { name: "orange", isTrue: false }, { name: "green", isTrue: false }, { name: "blue", isTrue: true }, { name: "all of the above", isTrue: false } ];
